35ca1ad67bf1564193b695142db8551d1829ba44
Delara12 <delarageralyn12@gmail.com>
2024-09-26 13:44:54 +0800

    Add files via upload

---
+++ controller/geralynController.js
@@ -0,0 +1,22 @@
+const gfd = {
+    index: (req,res) => {
+        res.render('index');
+    },
+    index: (req,res) => {
+        res.render('shop');
+    },
+    index: (req,res) => {
+        res.render('aboutUs');
+    },
+    index: (req,res) => {
+        res.render('services');
+    },
+    index: (req,res) => {
+        res.render('blog');
+    },
+    index: (req,res) => {
+        res.render('contactUs');
+    },
+}
+
+module.exports = gfd;
